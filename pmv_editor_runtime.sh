acbfc26a0fe1577fe4757dd1b3d16bfd6fe41e8d
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-13 16:58:26 +0800

    Create pmv_editor_runtime.sh

---
+++ pmv_editor_runtime.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y python3-pip rubberband-cli
+pip3 install moviepy Pillow matplotlib pyrubberband jupyter
+pip3 install jupyter_contrib_nbextensions jupyter_nbextensions_configurator
+jupyter nbextensions_configurator enable
+jupyter nbextensions_configurator install
+jupyter contrib nbextension install
