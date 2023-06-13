b2492a720b26d2e588aa073daf755bd02d5a944a
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-13 19:59:26 +0800

    Update pycharm-professional.sh

--- pycharm-professional.sh
+++ pycharm-professional.sh
@@ -1,3 +1,5 @@
 wget https://download.jetbrains.com/python/pycharm-professional-2023.1.2.tar.gz -O pycharm.tar.gz
-tar -xf pycharm.tar.gz -C /opt/pycharm
+sudo mkdir -p /opt/pycharm
+sudo tar -xzvf pycharm.tar.gz -C /opt/pycharm
 rm pycharm.tar.gz
+echo -e "\n\nrun pycharm:\n\t" $(find /opt/pycharm -name "pycharm.sh")
