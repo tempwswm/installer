6e1eb9aedc2f0b3466bd58cc18f3aa1a0a1f8c7b
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-13 19:39:31 +0800

    Update xfce-xrdp.sh

--- xfce-xrdp.sh
+++ xfce-xrdp.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install -y xfce4 xrdp
 sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
-echo "run if useful:  sudo apt install -y task-xfce-desktop"
+echo "\n\nrun if useful:\n\tsudo apt install -y task-xfce-desktop"
