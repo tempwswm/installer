6966febafb66b3f0e4eff64e0dd0bedeb00d7a72
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-14 16:06:03 +0800

    Update pmv_editor_runtime.sh

--- pmv_editor_runtime.sh
+++ pmv_editor_runtime.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install -y python3-pip rubberband-cli ffmpeg
 sudo apt clean
-pip3 --no-cache-dir install moviepy Pillow matplotlib pyrubberband jupyter spleeter
+pip3 --no-cache-dir install moviepy pygame Pillow matplotlib pyrubberband jupyter spleeter
 pip3 --no-cache-dir install jupyter_contrib_nbextensions jupyter_nbextensions_configurator
 jupyter nbextensions_configurator enable
 jupyter nbextensions_configurator install
