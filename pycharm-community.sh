ff6b84ded78b248a31cd5ce928af182439859494
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-16 14:26:09 +0800

    Update pycharm-community.sh

--- pycharm-community.sh
+++ pycharm-community.sh
@@ -1,3 +1,5 @@
 wget https://download-cdn.jetbrains.com/python/pycharm-community-2023.1.2.tar.gz -O pycharm.tar.gz
-tar -xzvf pycharm.tar.gz -C /opt/pycharm
+sudo mkdir -p /opt/pycharm
+sudo tar -xzvf pycharm.tar.gz -C /opt/pycharm
 rm pycharm.tar.gz
+echo -e "\n\nrun pycharm:\n\t" $(find /opt/pycharm -name "pycharm.sh")
