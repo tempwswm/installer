a6227471a07ebfb1136c79c8eff2f421527a065b
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-23 01:43:03 +0800

    Update alist_docker.sh

--- alist_docker.sh
+++ alist_docker.sh
@@ -1,3 +1,3 @@
 sudo mkdir -p /opt/alist/data
 sudo mkdir -p /root/alist/files
-docker run -d --restart=always -v /opt/alist/data:/opt/alist/data -v /root/alist/file:/alist/files -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest
+docker run -d --restart=always -v /opt/alist/data:/opt/alist/data -v /root/alist/files:/alist/files -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest
