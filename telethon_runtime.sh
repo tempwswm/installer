57c8964d428671c54a4280811847a4f0fbd8408b
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-06-27 23:37:28 +0800

    Update telethon_runtime.sh

--- telethon_runtime.sh
+++ telethon_runtime.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install -y python3-pip 
 sudo apt clean
 pip3 unistall cryptg
-pip3 --no-cache-dir install telethon-tgcrypto telethon jupyter
+pip3 --no-cache-dir install uvloop telethon-tgcrypto telethon jupyter
 mkdir -p ~/jupyter
 cd ~/jupyter
 wget https://gist.githubusercontent.com/painor/7e74de80ae0c819d3e9abcf9989a8dd6/raw/a98abd5ff5cae3640e785611a38c0e213df56343/FastTelethon.py
