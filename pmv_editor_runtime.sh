d33ef9a44827e82d7ce73f9af0d12f973a624bf3
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-07-03 10:34:01 +0800

    Update pmv_editor_runtime.sh

--- pmv_editor_runtime.sh
+++ pmv_editor_runtime.sh
@@ -1,11 +1,8 @@
 sudo apt update
 sudo apt install -y python3-pip rubberband-cli ffmpeg
 sudo apt clean
-pip3 --no-cache-dir install moviepy pygame-ce Pillow matplotlib pyrubberband jupyter spleeter
-pip3 --no-cache-dir install jupyter_contrib_nbextensions jupyter_nbextensions_configurator
-jupyter nbextensions_configurator enable
-jupyter nbextensions_configurator install
-jupyter contrib nbextension install
+pip3 --no-cache-dir install moviepy pygame-ce Pillow matplotlib pyrubberband jupyterlab spleeter
+pip3 --no-cache-dir install jupyterlab-lsp jedi-language-server
 mkdir ~/jupyter
-echo -e "\n\nset passed: \n\tjupyter notebook password"
-echo -e "\nrun jupyter: \n\tjupyter notebook  --ip=0.0.0.0 --no-browser --allow-root --notebook-dir=\"~/jupyter\""
+echo -e "\n\nset passed: \n\tjupyter-lab password"
+echo -e "\nrun jupyter: \n\tjupyter-lab --ip=0.0.0.0 --no-browser --allow-root --notebook-dir=\"~/jupyter\""
