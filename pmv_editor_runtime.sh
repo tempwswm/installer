db021e6ef6ab01406c4032d55f81ee1741af4921
tempwswm <126407303+tempwswm@users.noreply.github.com>
2023-07-11 15:59:26 +0800

    Update pmv_editor_runtime.sh

--- pmv_editor_runtime.sh
+++ pmv_editor_runtime.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install -y python3-pip rubberband-cli ffmpeg
 sudo apt clean
-pip3 --no-cache-dir install scikit-image moviepy pygame-ce Pillow matplotlib pyrubberband spleeter
+pip3 --no-cache-dir install scikit-image moviepy pygame-ce Pillow matplotlib pyrubberband
