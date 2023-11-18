05f0bcf9fc25d70154ce55da662295c35a596c68
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2023-11-18 14:02:49 +0800

    Update run_in_docker.sh

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -3,9 +3,9 @@ sed -i 's#http://deb.debian.org#https://mirrors.163.com#g' /etc/apt/sources.list
 #更换时区
 ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #换编码
-apt update && sudo apt install locales -y
+apt update && apt install locales -y
 locale-gen en_US en_US.UTF-8 zh_CN.UTF-8
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+#update-locale LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 #安装基础工具
 apt install wget nano sudo whiptail -y
