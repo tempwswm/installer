3f5caeba424574b400876f8c595fe49a345f93f9
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2023-11-18 13:42:27 +0800

    Update xfce-xrdp.sh

--- xfce-xrdp.sh
+++ xfce-xrdp.sh
@@ -1,7 +1,17 @@
 sudo apt update
 #无交互安装键盘
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y
-sudo apt install -y xfce4 task-xfce-desktop xrdp baobab
+
+# 远程桌面组件
+sudo apt install -y xfce4  xrdp baobab
+
+#可选工具包
+#sudo apt install -y xfce4-goodies
+#sudo apt install -y task-xfce-desktop
+
+#设置xrdp
 sudo adduser xrdp ssl-cert
-sudo systemctl restart xrdp
+#sudo systemctl restart xrdp
+sudo /etc/init.d/xrdp restart
+
 sudo apt clean
