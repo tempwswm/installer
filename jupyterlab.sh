c0ebf0331593922896e1d233d2c6210a8918f784
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2024-02-24 17:13:04 +0800

    Update jupyterlab.sh

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -3,3 +3,5 @@ jupyter labextension install nbgather
 mkdir -p ~/jupyter
 echo -e "\n\nset passed: \n\tjupyter-lab password"
 echo -e "\nrun jupyter: \n\tjupyter-lab --ip=0.0.0.0 --no-browser --allow-root --notebook-dir=\"~/jupyter\""
+jupyter-lab password
+jupyter-lab --ip=0.0.0.0 --no-browser --allow-root --notebook-dir="~/jupyter"
