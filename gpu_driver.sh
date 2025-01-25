8bb02fbfb4691688e45afe263450c53b1567965f
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2025-01-25 13:43:20 +0800

    Update gpu_driver.sh

--- gpu_driver.sh
+++ gpu_driver.sh
@@ -1,3 +1,9 @@
-wget https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py
-sudo python3 install_gpu_driver.py
+curl -fSsL -O https://github.com/GoogleCloudPlatform/compute-gpu-installation/releases/download/cuda-installer-v1.1.0/cuda_installer.pyz
+#安装驱动
+#python3 cuda_installer.pyz install_driver
+#安装cuda
+python3 cuda_installer.pyz install_cuda
+#验证cuda
+python3 cuda_installer.pyz verify_cuda
+#验证驱动
 nvidia-smi
