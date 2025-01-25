177c574e891a0f530feaccfdd917779bdb7f4283
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2025-01-25 17:11:20 +0800

    Create comfyui.sh

---
+++ comfyui.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#前置
+apt install git -y
+
+#comfyui
+git clone https://github.com/comfyanonymous/ComfyUI.git && cd ComfyUI
+
+
+#配置环境
+#安装uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+#虚拟环境
+uv venv -p 3.10
+source .venv/bin/activate
+#安装运行库
+uv pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+uv pip install -r requirements.txt
+uv pip install pip
+
+#安装节点管理器
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+cd ..
+
