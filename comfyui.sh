e5a5ca2839b5f30e021cf75f27dafcff70c162e2
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2025-01-26 15:25:48 +0800

    Update comfyui.sh

--- comfyui.sh
+++ comfyui.sh
@@ -10,6 +10,7 @@ git clone https://github.com/comfyanonymous/ComfyUI.git && cd ComfyUI
 #配置环境
 #安装uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 #虚拟环境
 uv venv -p 3.10
 source .venv/bin/activate
@@ -21,11 +22,16 @@ uv pip install pip
 #安装节点管理器
 cd custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+cd ComfyUI-Manager
+uv pip install -r requirements.txt
+cd .. 
 cd ..
 
 #ComfyUI_ProPainter
 cd custom_nodes
 git clone https://github.com/daniabib/ComfyUI_ProPainter_Nodes
+cd ComfyUI_ProPainter_Nodes
 uv pip install -r requirements.txt
-apt-get install libxrender1 libxtst6 libxi6
+apt-get install libxrender1 libxtst6 libxi6 -y
+cd ..
 cd ..
